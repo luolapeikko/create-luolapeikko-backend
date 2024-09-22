ee9ac2987e92470595b6bcbf778e3237ab530e73
Marko Harjula <marko.harjula@avanio.com>
2024-09-22 13:21:19 +0300

    wait bit hono to get service up

--- test/common.ts
+++ test/common.ts
@@ -1,1 +1,5 @@
 process.env.NODE_ENV = 'test';
+
+export function sleep(ms: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
--- test/honoStarter.test.ts
+++ test/honoStarter.test.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import './common.js';
 import {describe, expect, it} from 'vitest';
 import {startAll, stopAll} from 'hono-starter';
+import {sleep} from './common.js';
 
 process.env.PORT = '9898';
 
 describe('hono-starter', () => {
-	it('should start hono-starter', async () => {
+	it('should start hono-starter', async function () {
 		await expect(startAll()).resolves.toBe(undefined);
+		await sleep(500);
 	});
 	it('should get hello world json', async () => {
 		const res = await fetch('http://127.0.0.1:9898/api/hello');
